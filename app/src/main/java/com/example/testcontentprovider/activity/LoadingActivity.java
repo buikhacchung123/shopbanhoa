df1049a1f01c554d00bd5562a8905554017b3a68
Uyen Nguyen <uyen.nguyen@s3corp.com.vn>
2022-06-09 08:20:11 +0700

    Apply firebase for shop infor

--- app/src/main/java/com/example/testcontentprovider/activity/LoadingActivity.java
+++ app/src/main/java/com/example/testcontentprovider/activity/LoadingActivity.java
@@ -1,11 +1,10 @@
 package com.example.testcontentprovider.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import com.example.testcontentprovider.R;
 import com.example.testcontentprovider.adapter.DMSPAdapter;
 import com.example.testcontentprovider.data.ApiService;
@@ -13,9 +12,17 @@ import com.example.testcontentprovider.data.Constance;
 import com.example.testcontentprovider.data.RetrofitClient;
 import com.example.testcontentprovider.model.DanhMuc;
 import com.example.testcontentprovider.model.KhachHang;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.snapshot.ChildKey;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,6 +31,11 @@ public class LoadingActivity extends AppCompatActivity {
     private ApiService apiService;
     public static List<DanhMuc> arrayDM;
     public static List<KhachHang> arrayKH;
+    public static String Description1;
+    public static String Description2;
+    public static String Description3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +43,11 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
 
         apiService = RetrofitClient.getClient(Constance.API_URL).create(ApiService.class);
-        LoadingDanhMucHomePage();
-        LoadingAllKhachHang();
+        LoadingAllDanhMucs();
+        LoadingAllKhachHangs();
+        LoadDuLieuFirebase();
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -43,7 +58,7 @@ public class LoadingActivity extends AppCompatActivity {
             }
         }, 4000);
     }
-    private void LoadingDanhMucHomePage() {
+    private void LoadingAllDanhMucs() {
         Call<List<DanhMuc>> call = apiService.getAllDanhMucs();
         call.enqueue(new Callback<List<DanhMuc>>() {
             @Override
@@ -57,7 +72,7 @@ public class LoadingActivity extends AppCompatActivity {
             }
         });
     }
-    private void LoadingAllKhachHang() {
+    private void LoadingAllKhachHangs() {
         Call<List<KhachHang>> call = apiService.getAllKhachHangs();
         call.enqueue(new Callback<List<KhachHang>>() {
             @Override
@@ -67,6 +82,54 @@ public class LoadingActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<KhachHang>> call, Throwable t) {
+
+            }
+        });
+    }
+    public void LoadDuLieuFirebase(){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef1 = database.getReference("Description1");
+        DatabaseReference myRef2 = database.getReference("Description2");
+        DatabaseReference myRef3 = database.getReference("Description3");
+
+        //myRef1.setValue("Sứ mệnh của chúng tôi là giúp bạn trao đi tâm tư và biến mọi dịp trọng đại của bạn trở nên đặc biệt hơn.");
+        //myRef2.setValue("Đội ngũ giàu kinh nghiệm của chúng tôi luôn đảm bảo đem đến cho bạn những bó hoa tươi nhất được gói cùng các loại nguyên vật liệu chất lượng cao. Chúng tôi cung cấp nhiều chủng loại hoa, kích thước và thiết kế khác nhau phù hợp cho bất kỳ dịp nào bạn cần.");
+        //myRef3.setValue("Thông qua nền tảng trực tuyến, chúng tôi mong muốn đem đến cho bạn trải nghiệm thật tiện lợi, dễ dàng và tràn đầy niềm vui.");
+
+
+
+        // Read from the database
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot snapshot) {
+                Description1 = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot snapshot) {
+                Description2 = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        myRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot snapshot) {
+                Description3 = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
--- app/src/main/java/com/example/testcontentprovider/activity/ShopInforActivity.java
+++ app/src/main/java/com/example/testcontentprovider/activity/ShopInforActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import com.example.testcontentprovider.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class ShopInforActivity extends AppCompatActivity {
     Button btnHome;
     Toolbar toolbar;
+    TextView txtPara1, txtPara2, txtPara3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,19 @@ public class ShopInforActivity extends AppCompatActivity {
 
         btnHome = findViewById(R.id.btnHome);
         toolbar = findViewById(R.id.toolbar_shopinfor);
+        txtPara1 = findViewById(R.id.txtPara1);
+        txtPara2 = findViewById(R.id.txtPara2);
+        txtPara3 = findViewById(R.id.txtPara3);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Gán dữ liệu từ Firebase
+        txtPara1.setText(LoadingActivity.Description1);
+        txtPara2.setText(LoadingActivity.Description2);
+        txtPara3.setText(LoadingActivity.Description3);
+
+
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
